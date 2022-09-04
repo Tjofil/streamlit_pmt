69a89bbcdd814e084dce51b702afbbb044589027
Tjofil <drobnjakovicfilip@gmail.com>
2022-09-05 01:21:38 +0200

    fully functional

--- explore.py
+++ explore.py
@@ -1,4 +1,3 @@
-from turtle import width
 import streamlit as st
 import pandas as pd
 import requests
@@ -13,7 +12,7 @@ plt.style.use("fivethirtyeight")
 
 
 @st.cache
-def load_data(nrows):
+def load_data():
     raw_data = requests.get('https://desolate-oasis-06152.herokuapp.com/dataset')
     open('local.csv', 'wb').write(raw_data.content)
     data = pd.read_csv('local.csv')
@@ -30,48 +29,65 @@ def display_explore_page():
         """
     )
     data_load_state = st.text('Loading data...')
-    data = load_data(50000)
+    data = load_data()
     data = data.drop('Unnamed: 0', axis = 1)
     data_load_state.text('Finished fetching data!')
     st.subheader('Raw data')
-    st.write(data, width=100)
+    st.write(data, width=1000)
     
-    hist_data = [data['BMI'], data['Age']]
-    group_labels = ['BMI', 'Age']
-    # fig = px.scatter(x=[0, 1, 2, 3, 4], y=[0, 1, 4, 9, 16])
-
-    categorical_val = [column for column in data.columns if len(data[column].unique()) <= 5]
-    continous_val = [column for column in data.columns if len(data[column].unique()) > 5]
-
     import plotly.graph_objects as go
-    fig = go.Figure()
-    fig.add_trace(go.Histogram(x=data[data['Diabetes_binary'] == 0]['BMI'], name = 'Have diabetes = NO'))
-    fig.add_trace(go.Histogram(x=data[data['Diabetes_binary'] == 1]['BMI'], name = 'Have diabetes = YES'))
-    fig.update_layout(barmode='overlay')
-    fig.update_traces(opacity=0.75)
+    fig_bmi = go.Figure()
+    fig_bmi.add_trace(go.Histogram(x=data[data['Diabetes_binary'] == 0]['BMI'], name = 'Have diabetes = NO'))
+    fig_bmi.add_trace(go.Histogram(x=data[data['Diabetes_binary'] == 1]['BMI'], name = 'Have diabetes = YES'))
+    fig_bmi.update_layout(barmode='overlay', xaxis_title_text='Body mass index', yaxis_title_text='Count')
+    fig_bmi.update_traces(opacity=0.6)
 
     st.subheader('Diabetes cases per BMI distribution')
-    st.plotly_chart(fig, use_container_width = True)
+    st.plotly_chart(fig_bmi, use_container_width = True)
     
-    # Let's make our correlation matrix a little prettier
+    fig_income = go.Figure()
+    fig_income.add_trace(go.Histogram(x=data[data['Diabetes_binary'] == 0]['Income'], name = 'Have diabetes = NO'))
+    fig_income.add_trace(go.Histogram(x=data[data['Diabetes_binary'] == 1]['Income'], name = 'Have diabetes = YES'))
+    fig_income.update_layout(barmode='overlay', xaxis_title_text='Income category', yaxis_title_text='Count')
+    fig_income.update_xaxes(type='category')
+    fig_income.update_traces(opacity=0.6)
+
+    st.subheader('Confirmed diabetes cases per household income category.')
+
+    st.plotly_chart(fig_income, use_container_width = True)
+    st.write(""">Household income categories are calculated as following:
+    Category: 1 -> less than 10k USD annual income.
+    Category: 2 -> less than 15k USD annual income.
+                    ...
+    Category: 7 -> less than 75k USD annaul income.
+    Category: 8 -> more or equal than 75k USD annual income.
+    """)
+    # Correlation matrix of all dataset columns
+
+    fig_mental = go.Figure()
+    fig_mental.add_trace(go.Histogram(x=data[data['Diabetes_binary'] == 0]['MentHlth'], name = 'Have diabetes = NO'))
+    fig_mental.add_trace(go.Histogram(x=data[data['Diabetes_binary'] == 1]['MentHlth'], name = 'Have diabetes = YES'))
+    fig_mental.update_layout(barmode='overlay', xaxis_title_text='Days with mental health issues during last month', yaxis_title_text='Count')
+    fig_mental.update_traces(opacity=0.6)
+
+    st.subheader('Confirmed diabetes cases per mental health issues in last 30 days')
+    st.plotly_chart(fig_mental, use_container_width = True)
+
+     
     corr_matrix = data.corr()
     fig_corr =px.imshow(corr_matrix,
-        labels = dict(color = 'Productivity'),
+        labels = dict(color = 'Correlation'),
         x = data.columns,
         y = data.columns
     )
-    st.subheader('Correlation of health attributes')    
+
+    st.subheader('Correlation of all dataset columns') 
     st.plotly_chart(fig_corr, use_container_width=True)
     
+
     
-    # fig, ax = plt.subplots(figsize=(15, 15))
-    # ax = sns.heatmap(corr_matrix,
-    #                  annot=True,
-    #                  linewidths=0.5,
-    #                  fmt=".2f",
-    #                  cmap="YlGnBu")
-    # bottom, top = ax.get_ylim()
-    # ax.set_ylim(bottom + 0.5, top - 0.5)
+    
+
     
 
 
--- predict.py
+++ predict.py
@@ -1,6 +1,9 @@
 import json
 import streamlit as st
 import requests as rq
+import plotly.express as px
+from explore import load_data
+import numpy as np
 
 def predict(features):
     return json.loads(rq.post('https://desolate-oasis-06152.herokuapp.com/predict', json=features).content)['Outcome']
@@ -8,13 +11,13 @@ def predict(features):
 
 def display_predict_page():
     st.title('Diabetes prediction using Machine Learning')
-    st.write('''### Plug in your health information to predict the disease.''')
-
+    st.write('''### Plug in your personal information to predict the disease.''')
+    sex = st.selectbox('Sex', ('Male', 'Female'))
     high_bp = st.checkbox('Do you have high blood preasure ?', value = False)
-    st.write('>Having 130 or more for systolic and 80 or more for diastolic blood pressure is considered having a high blood pressure')
-
+    st.write('>Having 130 or more for systolic and 80 or more for diastolic blood pressure is considered having a high blood pressure.')
+    chol_check = st.checkbox('Have you checked your blood cholesterol in last 5 years ?', value = False)
     high_chol = st.checkbox('Do you have high cholesterol ?', value = False)
-    st.write('>Having 240 or more for total cholesterol level is considered having a high cholesterol')
+    st.write('>Having 240 or more total blood cholesterol level is considered having a high cholesterol.')
     bmi = st.slider('Body mass index', max_value= 98, min_value= 12)
     st.write('>[Find about and calculate your BMI.](https://www.nhlbi.nih.gov/health/educational/lose_wt/BMI/bmicalc.htm)')
     smoker = st.checkbox('Have you smoked at least 100 cigarettes in your entire life ?', value = False)
@@ -24,35 +27,71 @@ def display_predict_page():
     fruits = st.checkbox('Do you consume at least one fuit per day ?', value = False)
     veggies = st.checkbox('Do you consume at least one vegetable per day ?', value = False)
     hvy_alcohol = st.checkbox('Do you practice heavy alcohol consumption ?', value = False)
-    diff_walk = st.checkbox('Do you any difficulties walking ?', value = False)
     st.write('>Consuming at least 14 for men and at least 7 for women alcoholic drinks per week is considered heavy alcohol consumption.')
+    healthcare = st.checkbox('Do you have any kind of health care coverage, including health insurance and prepaid plans?', value= False)
+    cost = st.checkbox('Was there a time in the past 12 months when you needed to see a doctor but could not because of cost? ', value=False)
+    gen_hlth = st.slider('You would say that in general your health is (on scale from 1 to 5)',  min_value=1, max_value=5, step = 1) 
+    diff_walk = st.checkbox('Do you any difficulties walking ?', value = False)
     age = st.number_input('Your age', value= 24)
     age_category = 1 if age<= 24 else 2 + (age - 25)//5
 
     menth_hlth = st.slider('Number days of poor mental health in previous 30 days', min_value = 0, max_value = 30)
     phys_htlh = st.slider('Number days of poor physical health in previous 30 days', min_value = 0, max_value = 30)
 
+    education = st.slider('Education level on scale 1-6', min_value=1, max_value=6, step = 1)
+    income = st.number_input('Your annual household income in $', value = 15000)
+    income_category = 1 + (income - 10000)//5000
+    if income_category < 1: income_category = 1
+    if income_category > 8: income_category = 8
+
     features = {
-    "HighBP":                 high_bp,
-    "HighChol":               high_chol,
-    "BMI":                    bmi,
-    "Smoker":                 smoker,
-    "Stroke":                 stroke,
-    "HeartDiseaseorAttack":   heart,
-    "PhysActivity":           phys_act,
-    "Fruits":                 fruits,
-    "Veggies":                veggies,
-    "HvyAlcoholConsump":      hvy_alcohol,
-    "MentHlth":               menth_hlth,
-    "PhysHlth":               phys_htlh,
-    "DiffWalk":               diff_walk,
-    "Age":                    age_category
+    "HighBP":  high_bp,
+    "HighChol":  high_chol,
+    "CholCheck":  chol_check,
+    "BMI":  bmi,
+    "Smoker":  smoker,
+    "Stroke":  stroke,
+    "HeartDiseaseorAttack":  heart,
+    "PhysActivity":  phys_act,
+    "Fruits":  fruits,
+    "Veggies":  veggies,
+    "HvyAlcoholConsump":  hvy_alcohol,
+    "AnyHealthcare":  healthcare,
+    "NoDocbcCost":  cost,
+    "GenHlth":  gen_hlth,
+    "MentHlth":  menth_hlth,
+    "PhysHlth":  phys_htlh,
+    "DiffWalk":  diff_walk,
+    "Sex":  1 if sex == 'Male' else 0,
+    "Age":  age_category,
+    "Education":  education,
+    "Income":  income_category
     }
 
     calc = st.button('Calculate the diabetes prediction')
     if calc:
         prediction = predict(features)
-        st.subheader(f'The model has predicted that you {"" if prediction else "dont"} have diabetes')
+        st.write(f'### Based on the model prediction you likely _{"" if prediction else "dont"}_ have diabetes')
+
+
+        data = load_data()
+        data = data.drop('Unnamed: 0', axis = 1)
+        data = data.rename(columns={'Diabetes_binary' : 'Class'})
+        features['Class'] = 2
+        data = data.append(dict(features), ignore_index=True)
+        data['Class'] = data['Class'].astype(str)
+        data = data.replace({"Class": {"1" : "Have diabetes", "0" : "Doesn't have diabetes", "2" : "You"}})
+        fig = px.scatter(data, x="BMI", y="Age", color="Class", hover_data=list(features.keys()))
+
+        st.plotly_chart(fig, use_container_width=True)
+
+        st.write(""">Age categories are calculated as following:
+    Category: 1 -> 24y old or younger.
+    Category: 2 -> 25-29y old.
+                    ...
+    Category: 12 -> 75-79y old.
+    Category: 8 -> more than 79 y old.
+    """)
 
 
 
